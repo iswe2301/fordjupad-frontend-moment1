0023849ddaae4dda34455ec6c00b42f7efc56432
Isa Westling <isawestling.blogg@hotmail.com>
2025-01-21 17:59:22 +0100

    Skapat en header-komponent. Skapat interface för headerkomponentens props (titel) samt lagt till inline styling och hederbild.

---
+++ ReactApp/src/components/Header.tsx
@@ -0,0 +1,35 @@
+import headerImage from "../assets/header.jpg"; // Importera bild för sidhuvudet
+
+// Interface för att definiera vilka props som ska skickas till komponenten
+interface HeaderProps {
+    title: string; // Titel för sidan
+}
+
+// Funktion för att visa sidhuvudet med en bild
+function Header({ title }: HeaderProps) {
+    return (
+        <header>
+            {/* Banner med titel, jsx-inline styling */}
+            <div style={{ backgroundColor: "#222021", padding: "1rem", textAlign: "center" }}>
+                <h1 style={{color: "#ffffff", margin: "0"}}>{title}</h1>
+            </div>
+
+            {/* Headerbild */}
+            <div style={{ width: "100%" }}>
+                <img
+                    src={headerImage}
+                    alt="Headerbild"
+                    style={{
+                        width: "100%",
+                        height: "auto",
+                        display: "block",
+                        maxHeight: "600px",
+                        objectFit: "cover"
+                    }}
+                />
+            </div>
+        </header>
+    );
+}
+
+export default Header; // Exportera header-komponenten
