d8d4d58edcfaf495e0db7e46a1578ac071b4a8ac
Isa Westling <isawestling.blogg@hotmail.com>
2025-01-21 17:58:14 +0100

    Skapat en footer-komponent med inline JSX styling, lagt till export

---
+++ ReactApp/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// Funktion för footer-komponent
+function Footer() {
+    {/* inline styling med JSX */ }
+    return (
+        <footer style={{ backgroundColor: "#cbc1ae", padding: "10px", textAlign: "center" }}>
+            <p>© 2025 Isa Westling - Mina resor</p>
+        </footer>
+    );
+}
+
+export default Footer; // Exportera footer-komponenten
